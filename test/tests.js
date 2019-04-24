f82cf3b94493fad8c052d524c6c676fdcd89ddc6
DannySuarez <suarezd10@gmail.com>
2019-04-24 14:29:47 -0700

    created passing test and function

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,21 @@
 const test = QUnit.test;
+function flipCoin(number) {
+    if(number < 0.5) {
+        return 'heads';
+    }
+    return 'tails';
+}
 
-test('')
+
+test('return heads if number is less than .5', assert => {
+
+    //arrange
+    const number = 0.3;
+    const expected = 'heads';
+
+    //act
+    const actual = flipCoin(number);
+    //assert
+    assert.equal(expected, actual);
+
+});
