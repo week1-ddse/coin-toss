562a3744df4209057a061eec092020e220b1a6b3
DannySuarez <suarezd10@gmail.com>
2019-04-24 15:15:31 -0700

    cleaned up unnececssary lines

--- test/tests.js
+++ test/tests.js
@@ -2,27 +2,21 @@ import flipCoin from '../src/flipCoin.js';
 const test = QUnit.test;
 
 test('return heads if number is less than .5', assert => {
-
     //arrange
     const number = 0.3;
     const expected = 'heads';
-
     //act
     const actual = flipCoin(number);
     //assert
     assert.equal(expected, actual);
-
 });
 
 test('return tails if number is equal to or more than .5', assert => {
-
     //arrange
     const number = 0.6;
     const expected = 'tails';
-
     //act
     const actual = flipCoin(number);
     //assert
     assert.equal(expected, actual);
-
 });
