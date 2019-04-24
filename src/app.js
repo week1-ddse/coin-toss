5431d9dcc30cd2426a8c6976562692ff07a2b486
DannySuarez <suarezd10@gmail.com>
2019-04-24 14:00:55 -0700

    defined variables and added event listener to app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const winCount = document.getElementById('win-count');
+const lossCount = document.getElementById('loss-count');
+const coinImage = document.getElementById('coin-image');
+const resultMessage = document.getElementById('result-message');
+const headsRadio = document.getElementById('heads-radio');
+const tailsRadio = document.getElementById('tails-radio');
+const flipButton = document.getElementById('flip-button');
+
+flipButton.addEventListener('click', () => {
+    console.log('clicked button')
+
+
+});
